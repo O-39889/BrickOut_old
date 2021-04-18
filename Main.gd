a5f9b286a1eb11fd2199d16dc700d895ce18d4f0
Nente <76152616+Nente520@users.noreply.github.com>
2021-04-18 15:37:06 +0300

    Add walls and basic Paddle functionality
    also some more tweaks and additions, such as adding groups and collision layers

--- Main.gd
+++ Main.gd
@@ -1,1 +1,24 @@
 extends Node2D;
+
+
+func init_walls() -> void:
+	$WallL/CollisionShape2D.shape.b.y = get_viewport_rect().size.y;
+	
+	$WallR/CollisionShape2D.shape.a.x = get_viewport_rect().size.x;
+	$WallR/CollisionShape2D.shape.b.x = get_viewport_rect().size.x;
+	$WallR/CollisionShape2D.shape.b.y = get_viewport_rect().size.y;
+	
+	$Ceiling/CollisionShape2D.shape.b.x = get_viewport_rect().size.x
+
+
+func reset_paddle() -> void:
+	var vec: Vector2 = Vector2((get_viewport_rect().size.x -
+			$Paddle/CollisionShape2D.shape.b.x) / 2,
+			get_viewport_rect().size.y - 30)
+	
+	$Paddle.position = vec;
+
+
+func _ready() -> void:
+	init_walls();
+	reset_paddle();
--- Paddle.gd
+++ Paddle.gd
@@ -1,4 +1,16 @@
 extends KinematicBody2D;
 
-func _physics_process(delta):
-	pass;
+
+const SPEED: int = 240;
+
+
+func _physics_process(delta) -> void:
+	var vel: Vector2 = Vector2.ZERO;
+	
+	if Input.is_action_pressed("right"):
+		vel.x += 1;
+	if Input.is_action_pressed("left"):
+		vel.x -= 1;
+	vel *= SPEED;
+	
+	move_and_collide(vel * delta);
