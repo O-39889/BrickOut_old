d9e067e6a173dc11dd2077c6c13d2fc72799f887
Nente <76152616+Nente520@users.noreply.github.com>
2021-05-02 15:53:28 +0300

    Add Global singleton
    also changed the color modulation of ball placeholder to make its size a bit more clear

---
+++ scripts/global.gd
@@ -0,0 +1,57 @@
+extends Node;
+# Singleton for various global scope constants and enums
+
+
+enum PaddleSizes {
+	PSIZE_SMALL,
+	PSIZE_REGULAR,
+	PSIZE_LARGE,
+	PSIZE_MAX,
+	PSIZE_DEFAULT = 1,
+};
+
+enum BallSizes {
+	BSIZE_SMALL,
+	BSIZE_REGULAR,
+	BSIZE_LARGE,
+	BSIZE_MAX,
+	BSIZE_DEFAULT = 1,
+};
+
+enum BallSpeeds {
+	BSPEED_VERY_SLOW,
+	BSPEED_SLOW,
+	BSPEED_REGULAR,
+	BSPEED_FAST,
+	BSPEED_VERY_FAST,
+	BSPEED_MAX,
+	BSPEED_DEFAULT = 2,
+};
+
+const PADDLE_SIZES: Array = [
+	150,
+	200,
+	240,
+];
+
+const BALL_SIZES: Array = [
+	10,
+	15,
+	25,
+];
+
+const BALL_SPEEDS: Array = [
+	190,
+	250,
+	300,
+	350,
+	420,
+];
+
+var paddle_current_size: int = PADDLE_SIZES[PaddleSizes.PSIZE_DEFAULT];
+var ball_current_size: int = BALL_SIZES[BallSizes.BSIZE_DEFAULT];
+# this one will be very important
+var ball_current_speed: int = BALL_SPEEDS[BallSpeeds.BSPEED_DEFAULT];
+
+# why not just use class_name (if it works as I expect) and put constants
+# in their respective nodes?
